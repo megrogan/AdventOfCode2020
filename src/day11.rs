0fdd931c2ae9f8429f8cfc5f2b71ab11c0c3b0f8
megrogan <megrogan@gmail.com>
2020-12-12 00:55:36 +0000

    Part 2 not quite working yet...

--- src/day11.rs
+++ src/day11.rs
@@ -7,17 +7,17 @@ pub fn run() -> (usize, usize) {
     let mut floor_plan = load_floor_plan(&input);
 
     (
-        count_occupied_in_steady_state(&mut floor_plan),
-        0
+        count_occupied_in_steady_state(&mut floor_plan, &AdjacentPolicy{}),
+        count_occupied_in_steady_state(&mut floor_plan, &VisiblePolicy{}),
     )
 }
 
-fn count_occupied_in_steady_state(floor_plan: &mut Vec<Vec<Space>>) -> usize {
+fn count_occupied_in_steady_state(floor_plan: &mut Vec<Vec<Space>>, seating_policy: &dyn SeatingPolicy) -> usize {
 
     for i in 0..99999 {
         let mut new_floor_plan = floor_plan.clone();
 
-        if !play_round(&floor_plan, &mut new_floor_plan) {
+        if !play_round(&floor_plan, &mut new_floor_plan, seating_policy) {
             return count_occupied(&new_floor_plan);
         }
 
@@ -31,33 +31,7 @@ fn count_occupied_in_steady_state(floor_plan: &mut Vec<Vec<Space>>) -> usize {
     panic!("Unstable floor plan!");
 }
 
-fn play_round(floor_plan: &Vec<Vec<Space>>, new_floor_plan: &mut Vec<Vec<Space>>) -> bool {
-
-    fn adjacent_spaces(floor_plan: &Vec<Vec<Space>>, x: usize, y:usize) -> Vec<Space> {
-        let mut seats = Vec::new();
-        seats.push(floor_plan[y-1][x-1]);
-        seats.push(floor_plan[y-1][x]);
-        seats.push(floor_plan[y-1][x+1]);
-        seats.push(floor_plan[y][x-1]);
-        seats.push(floor_plan[y][x+1]);
-        seats.push(floor_plan[y+1][x-1]);
-        seats.push(floor_plan[y+1][x]);
-        seats.push(floor_plan[y+1][x+1]);
-        seats
-    }
-
-    fn adjacent_seat_is_occupied(floor_plan: &Vec<Vec<Space>>, x: usize, y:usize) -> bool {
-        adjacent_spaces(floor_plan, x, y)
-            .iter()
-            .any(|space| *space == Space::Occupied)
-    }
-
-    fn more_than_3_adjacent_seats_occupied(floor_plan: &Vec<Vec<Space>>, x: usize, y:usize) -> bool {
-        adjacent_spaces(floor_plan, x, y)
-            .iter()
-            .filter(|space| **space == Space::Occupied)
-            .count() > 3
-    }
+fn play_round(floor_plan: &Vec<Vec<Space>>, new_floor_plan: &mut Vec<Vec<Space>>, seating_policy: &dyn SeatingPolicy) -> bool {
 
     let mut has_changed = false;
 
@@ -68,8 +42,20 @@ fn play_round(floor_plan: &Vec<Vec<Space>>, new_floor_plan: &mut Vec<Vec<Space>>
 
             let space = match space {
                 Space::Floor => Space::Floor,
-                Space::Empty => if adjacent_seat_is_occupied(&floor_plan, x, y) { Space::Empty } else { has_changed = true; Space::Occupied },
-                Space::Occupied => if more_than_3_adjacent_seats_occupied(&floor_plan, x, y) { has_changed = true; Space::Empty } else { Space::Occupied }
+                Space::Empty => 
+                    if seating_policy.should_become_occupied(&floor_plan, x, y) { 
+                        has_changed = true; 
+                        Space::Occupied 
+                    } else { 
+                        Space::Empty 
+                    },
+                Space::Occupied => 
+                    if seating_policy.should_become_empty(&floor_plan, x, y) { 
+                        has_changed = true; 
+                        Space::Empty 
+                    } else { 
+                        Space::Occupied 
+                    }
             };
 
             new_floor_plan[y][x] = space;
@@ -85,6 +71,95 @@ fn count_occupied(floor_plan: &Vec<Vec<Space>>) -> usize {
         .flatten()
         .filter(|s| **s == Space::Occupied)
         .count()
+}
+
+trait SeatingPolicy {
+    fn should_become_empty(&self, floor_plan: &Vec<Vec<Space>>, x: usize, y: usize) -> bool;
+    fn should_become_occupied(&self, floor_plan: &Vec<Vec<Space>>, x: usize, y: usize) -> bool;
+}
+
+struct AdjacentPolicy {}
+
+impl AdjacentPolicy {
+    fn adjacent_occupied_seats(floor_plan: &Vec<Vec<Space>>, x: usize, y:usize) -> usize {
+
+        let directions = vec![
+            (y-1, x-1),
+            (y-1, x),
+            (y-1, x+1),
+            (y, x-1),
+            (y, x+1),
+            (y+1, x-1),
+            (y+1, x),
+            (y+1, x+1),
+        ];
+
+        directions
+            .iter()
+            .map(|d| floor_plan[d.0][d.1])
+            .filter(|s| *s == Space::Occupied)
+            .count()
+    }    
+}
+
+impl SeatingPolicy for AdjacentPolicy {
+
+    fn should_become_empty(&self, floor_plan: &Vec<Vec<Space>>, x: usize, y: usize) -> bool {
+        AdjacentPolicy::adjacent_occupied_seats(floor_plan, x, y) >= 4
+    }
+
+    fn should_become_occupied(&self, floor_plan: &Vec<Vec<Space>>, x: usize, y: usize) -> bool {
+        AdjacentPolicy::adjacent_occupied_seats(floor_plan, x, y) < 1
+    }
+}
+
+struct VisiblePolicy {}
+
+impl VisiblePolicy {
+
+    fn first_seat_in_direction(floor_plan: &Vec<Vec<Space>>, mut x: usize, mut y:usize, dx: i8, dy: i8) -> Space {
+        while x < floor_plan[0].len()-1 && y < floor_plan.len()-1 && x > 0 && y > 0 {
+            if dx < 0 {x -= dx.abs() as usize;} else {x += dx as usize;}
+            if dy < 0 {y -= dy.abs() as usize;} else {y += dy as usize;}
+            let space = floor_plan[y][x];
+            if space != Space::Floor {
+                return space;
+            }
+        }
+
+        Space::Floor
+    }
+
+    fn visible_occupied_seats(floor_plan: &Vec<Vec<Space>>, x: usize, y:usize) -> usize {
+
+        let directions = vec![
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, -1),
+            (0, 1),
+            (1, -1),
+            (1, 0),
+            (1, 1),
+        ];
+
+        directions
+            .iter()
+            .map(|d| VisiblePolicy::first_seat_in_direction(floor_plan, x, y, d.1, d.0))
+            .filter(|s| *s == Space::Occupied)
+            .count()
+    }    
+}
+
+impl SeatingPolicy for VisiblePolicy {
+
+    fn should_become_empty(&self, floor_plan: &Vec<Vec<Space>>, x: usize, y: usize) -> bool {
+        VisiblePolicy::visible_occupied_seats(floor_plan, x, y) >= 5
+    }
+
+    fn should_become_occupied(&self, floor_plan: &Vec<Vec<Space>>, x: usize, y: usize) -> bool {
+        VisiblePolicy::visible_occupied_seats(floor_plan, x, y) < 1
+    }
 }
 
 #[derive(Copy, Clone, Debug, PartialEq)]
@@ -167,7 +242,7 @@ mod tests {
 
         let mut floor_plan = load_floor_plan(&input);
         
-        let result = count_occupied_in_steady_state(&mut floor_plan);
+        let result = count_occupied_in_steady_state(&mut floor_plan, &AdjacentPolicy{});
 
         assert_eq!(37, result);
     }
